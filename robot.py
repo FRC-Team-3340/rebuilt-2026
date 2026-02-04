d553c8cf7cee5bed5eed6d6138670e89c9d992f3
Team 3340 <131303875+team3340@users.noreply.github.com>
2026-02-04 16:39:23 -0500

    [2] - pls work

--- robot.py
+++ robot.py
@@ -5,6 +5,8 @@ import wpilib.drive
 import phoenix6
 import rev
 
+from phoenix6 import hardware
+
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         print("""
@@ -18,11 +20,11 @@ class MyRobot(wpilib.TimedRobot):
 
         """)
 
-        self.motors = self.setup_neos([2])
+        self.motors = self.setup_talons([2])
         self.controller = wpilib.XboxController(0)
         self.stick = wpilib.Joystick(0)
 
-        self.first_motor = self.motors[2]
+        self.first_motor = wpilib.Talon(2)
         
         pass
 
@@ -40,14 +42,30 @@ class MyRobot(wpilib.TimedRobot):
         
         print(f"[!] Succesfully init'ed NEOs on CAN IDs: {list(neo_dict.keys())}")
         return neo_dict
+    
+    def setup_talons(self, can_ids: list):
+        """
+        Inits a NEO dictionary with given CAN IDs
+        """
+
+        talon_dict = {}
+
+        for can_id in can_ids:
+            motor = hardware.TalonFX(can_id)
+            
+            talon_dict[can_id] = motor
+        
+        print(f"[!] Succesfully init'ed Talons on CAN IDs: {list(talon_dict.keys())}")
+        return talon_dict
 
     def testPeriodic(self):
-        speed = -self.stick.getRawAxis(1)
+        """speed = -self.stick.getRawAxis(0)
 
         if abs(speed) < 0.1: # don't move if move is slight
             speed = 0
-            print('joystick speed too small')
-        else:
-            print('joystick speed good enough')
-            self.first_motor.set(speed)
+        
+        self.first_motor.set(speed)
         pass
+        speed = 0.5"""
+        self.first_motor.set(0.5)
+
