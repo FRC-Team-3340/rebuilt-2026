b4b8e2523cbada2858c7355d11d818d6e22d2023
Team 3340 <131303875+team3340@users.noreply.github.com>
2026-02-10 15:15:53 -0500

    functional robotpy
    works with 2026 robotpy, 2026 roborio and sparkmax + talon

--- robot.py
+++ robot.py
@@ -1,71 +1,37 @@
-# TODO: insert robot code here
-
 import wpilib
-import wpilib.drive
-import phoenix6
-import rev
-
-from phoenix6 import hardware
+import phoenix5
+from wpilib import Joystick
+import rev    
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        print("""
+        self.talon1 = phoenix5.WPI_TalonSRX(3)
+        self.sparkmax1 = rev.SparkMax(2, rev.SparkLowLevel.MotorType.kBrushless)
+        self.joystick = Joystick(0)
 
-        |
-        |
-        |        REBUILT -- 2026
-        |            by yes
-        |
-        |
-
-        """)
-
-        self.motors = self.setup_talons([2])
-        self.controller = wpilib.XboxController(0)
-        self.stick = wpilib.Joystick(0)
-
-        self.first_motor = wpilib.Talon(2)
-        
-        pass
-
-    def setup_neos(self, can_ids: list):
-        """
-        Inits a NEO dictionary with given CAN IDs
-        """
-
-        neo_dict = {}
-
-        for can_id in can_ids:
-            motor = rev.SparkMax(can_id, rev.SparkLowLevel.MotorType.kBrushless)
-            
-            neo_dict[can_id] = motor
-        
-        print(f"[!] Succesfully init'ed NEOs on CAN IDs: {list(neo_dict.keys())}")
-        return neo_dict
+    def teleopPeriodic(self):
+        right_trigger = self.joystick.getRawAxis(3)  # Right trigger for motor speed
+        left_trigger = self.joystick.getRawAxis(2)
+        self.sparkmax1.set(right_trigger / 10)
+        self.talon1.set(phoenix5.ControlMode.PercentOutput, left_trigger / 10)
+        print(right_trigger, left_trigger)
     
-    def setup_talons(self, can_ids: list):
-        """
-        Inits a NEO dictionary with given CAN IDs
-        """
-
-        talon_dict = {}
-
-        for can_id in can_ids:
-            motor = hardware.TalonFX(can_id)
-            
-            talon_dict[can_id] = motor
-        
-        print(f"[!] Succesfully init'ed Talons on CAN IDs: {list(talon_dict.keys())}")
-        return talon_dict
+    def testInit(self):
+        """This function is called once each time the robot enters test mode."""
 
     def testPeriodic(self):
-        """speed = -self.stick.getRawAxis(0)
+        """This function is called periodically during test mode."""
 
-        if abs(speed) < 0.1: # don't move if move is slight
-            speed = 0
-        
-        self.first_motor.set(speed)
-        pass
-        speed = 0.5"""
-        self.first_motor.set(0.5)
+    def teleopInit(self):
+        """This function is called once each time the robot enters teleoperated mode."""
+    
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
 
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
