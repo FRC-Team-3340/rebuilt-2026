6235b5e079eb7fa66e225e6c3c9eb60b95e085a6
Alexander Suazo <alexander.g.suazo@gmail.com>
2026-02-12 16:18:00 -0500

    Update drive.py
    Add new imports for PathPlannerLib

--- modules/components/drive.py
+++ modules/components/drive.py
@@ -1,9 +1,13 @@
-from wpilib import MotorControllerGroup
+from wpilib import MotorControllerGroup, DriverStation
 
 from wpilib.drive import DifferentialDrive
 import wpilib.drive
 
 from modules.components.hardware.motor_controllers import TalonMotor as m
+
+from pathplannerlib.auto import AutoBuilder
+from pathplannerlib.controller import PPLTVController
+from pathplannerlib.config import RobotConfig
 
 
 class Drive(DifferentialDrive):
@@ -57,3 +61,27 @@ class Drive(DifferentialDrive):
         # super().__init__(leftMotor=front_left, rightMotor=front_right)
 
         self.setMaxOutput(maxOutput=Drive.MAX_POWER)
+
+        # Do all subsystem initialization here
+        # ...
+
+        # Load the RobotConfig from the GUI settings. You should probably
+        # store this in your Constants file
+        config = RobotConfig.fromGUISettings()
+
+        # Configure the AutoBuilder last
+        AutoBuilder.configure(
+            self.getPose, # Robot pose supplier
+            self.resetPose, # Method to reset odometry (will be called if your auto has a starting pose)
+            self.getRobotRelativeSpeeds, # ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            lambda speeds, feedforwards: self.driveRobotRelative(speeds), # Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also outputs individual module feedforwards
+            PPLTVController(0.02), # PPLTVController is the built in path following controller for differential drive trains
+            config, # The robot configuration
+            self.shouldFlipPath, # Supplier to control path flipping based on alliance color
+            self # Reference to this subsystem to set requirements
+        )
+    def shouldFlipPath():
+        # Boolean supplier that controls when the path will be mirrored for the red alliance
+        # This will flip the path being followed to the red side of the field.
+        # THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+        return DriverStation.getAlliance() == DriverStation.Alliance.kRed
